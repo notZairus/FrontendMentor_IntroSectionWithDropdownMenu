3c3cab5231fef24dc7d2c6875c853bc0d24d421c
Zairus Bermillo <zairusb12@gmail.com>
2024-04-20 22:42:27 +0700

    Finished, will add readme file tom.

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ document.body.addEventListener("mouseover", (event) => {
   let tgt = event.target;
   
   if (tgt.id == "feat" || tgt.id == "comp") {
+    tgt.nextElementSibling.src = "./images/icon-arrow-up.svg";
     tgt.parentNode.querySelector("ul").style.display = "flex";
   }
+
 })
 
 // let section = document.querySelector("header");
@@ -21,21 +23,78 @@ document.body.addEventListener("mouseover", (event) => {
 let lis = Array.from(document.querySelectorAll(".navii"));
 
 lis.forEach((li) => {
-  let hovering;
+  let Hovering = false;
 
   li.addEventListener("mouseover", () => {
-    hovering = true;
-  })
-  
-  li.addEventListener("mouseleave", () => {
-    hovering = false;
+    Hovering = true;
   })
 
-  li.addEventListener("mouseleave", (event) => {
+  li.addEventListener("mouseleave", () => {
+    Hovering = false;
+  })
+
+  li.addEventListener("mouseleave", () => {
     setTimeout(() => {
-      if (!hovering) {
+      if (!Hovering) {
         li.querySelector("ul").style.display = "none";
+        li.querySelector(".arrow").src = "./images/icon-arrow-down.svg";
       }
     }, 300)
   })
+
+})
+
+let bground = document.querySelector(".aside").querySelector("img");
+
+window.addEventListener("DOMContentLoaded", () => {
+  if (window.innerWidth < 427) {
+    bground.src = "./images/image-hero-mobile.png";
+  }
+})
+
+let sidebar = document.getElementById("sdbr");
+
+document.addEventListener("click", (event) => {
+  let tgt = event.target;
+  let sdbrContent = sidebar.querySelector(".sidecontent");
+  let dark = document.querySelector(".dark-blur");
+
+  if (tgt.id == "menu" || tgt.id == "menuimg")
+  {
+    sidebar.style.width = "270px";
+    sdbrContent.style.display = "block";
+    dark.style.display = "block";
+    dark.style.opacity = "1";
+  }
+  else if (tgt.id == "menucloseimg" || tgt.id == "dark-blur") {
+    sdbrContent.style.display = "none";
+    sidebar.style.width = "0px";
+    dark.style.display = "none";
+    dark.style.opacity = "0";
+  }
+})
+
+sidebar.addEventListener("click", (event) => {
+  let tgt = event.target;
+  let UnorderedList;
+
+  if (tgt.id == "featside" || tgt.id == "compside"){
+    let nextEm = tgt.nextElementSibling;
+    while (nextEm) {
+      if (nextEm.tagName === "UL") {
+        UnorderedList = nextEm;
+        break;
+      }
+      nextEm = nextEm.nextElementSibling;
+    }
+  }
+
+  if (UnorderedList.style.display == "none") {
+    UnorderedList.style.display = "flex";
+    tgt.nextElementSibling.src = "./images/icon-arrow-up.svg";
+  }
+  else {
+    UnorderedList.style.display = "none";
+    tgt.nextElementSibling.src = "./images/icon-arrow-down.svg";
+  }
 })
